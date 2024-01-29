b90a4c0eeb4bfef107a5246a2ba892e6899e625a
arfan277 <arfan2771@gmail.com>
2024-01-29 08:49:08 +0700

    modified .js onclick

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
+// function toggleMenu() {
+//     const menu = document.querySelector(".menu-links");
+//     const icon = document.querySelector(".hamburger-icon");
+//     menu.classList.toggle("open");
+//     icon.classList.toggle("open");
+//   }
   
+// Toggle menu function
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  const isOpen = menu.classList.contains("open");
+
+  // If the menu is already open, close it
+  if (isOpen) {
+      menu.classList.remove("open");
+      icon.classList.remove("open");
+  } else {
+      // If the menu is closed, open it
+      menu.classList.add("open");
+      icon.classList.add("open");
+      
+      // Add event listener to close menu when clicking anywhere outside of it
+      document.addEventListener("click", closeMenuOutside);
+  }
+}
+
+// Function to close menu when clicking anywhere outside of it
+function closeMenuOutside(event) {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+
+  if (!menu.contains(event.target) && !icon.contains(event.target)) {
+      menu.classList.remove("open");
+      icon.classList.remove("open");
+      document.removeEventListener("click", closeMenuOutside);
+  }
+}
